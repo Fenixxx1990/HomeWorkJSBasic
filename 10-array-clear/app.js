13a0abe88042329a640d7cb4172d8f6aff7ad487
Fenix <fenixxx1990@yandex.ru>
2026-02-02 13:35:33 +0400

    10-array-clear-2

--- 10-array-clear/app.js
+++ 10-array-clear/app.js
@@ -1,21 +1,23 @@
 function deleteArrayValue(array, fn) {
+  const newArray = [];
   for (let i = 0; i < array.length; i++) {
     if (fn(array[i])) {
-      console.log(array[i]);
-      array.splice(i, 1);
+      continue;
+    } else {
+      newArray.push(array[i]);
     }
   }
-  return array;
+  return newArray;
 }
 
 const array = [0, 1, -4, 1000, -122, 22];
 
 function notPositive(value) {
-  return value < 0;
+  return value <= 0;
 }
 
 function isPositive(value) {
-  return value > 0;
+  return value >= 0;
 }
 
 console.log(deleteArrayValue(array, notPositive));
