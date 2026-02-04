2e858025764f340ad859d0d1d2b990256c1f872c
Fenix <fenixxx1990@yandex.ru>
2026-02-05 02:17:06 +0400

    13-query

--- app.js
+++ app.js
@@ -1,6 +1,65 @@
-const originalText = "Привет мир, мир прекрасен";
-const searchString = "мир";
-const replaceString = "JavaScript";
+// const warehouse = {
+//   goods: [],
+//   findGoodById(id) {
+//     return this.goods.find((good) => good.id === id) || "Такого товара нет";
+//   },
+//   addGood(good) {
+//     if (this.findGoodById(good.id) !== "Такого товара нет") {
+//       console.log("Такой товар уже есть на складе");
+//       return false;
+//     } else {
+//       this.goods.push(good);
+//       return true;
+//     }
+//   },
+//   getWeightKg() {
+//     return this.goods.reduce((acc, good) => acc + (good?.weight?.kg || 0), 0);
+//   },
+// };
 
-const result = originalText.replace(searchString, replaceString);
-console.log(result.trimEnd());
+// // товары
+
+// const car = {
+//   id: 1,
+//   weight: {
+//     kg: 1000,
+//   },
+//   brand: "Ford",
+// };
+
+// const chair = {
+//   id: 2,
+//   weight: {
+//     kg: 2,
+//   },
+// };
+
+// const paper = {
+//   id: 3,
+//   color: "red",
+// };
+
+// console.log(warehouse.addGood(car));
+// console.log(warehouse.addGood(chair));
+// console.log(warehouse.addGood(paper));
+// console.log(warehouse.addGood(paper));
+// console.log(warehouse.findGoodById(1));
+// console.log(warehouse.findGoodById(2));
+// console.log(warehouse.findGoodById(3));
+// console.log(warehouse.getWeightKg());
+// console.log(warehouse);
+
+const obj = {
+  brand: "Toyota",
+  model: "Camry",
+  year: 2020,
+  getInfo() {
+    return `${this.brand} ${this.model} ${this.year}`;
+  },
+};
+
+console.log(obj.getInfo());
+
+for (const key of Object.keys(obj)) {
+  console.log(key);
+}
